424da42ced0f0cbee82a2cfe2327a6323bf0343b
Nadeem Gulam <68492875+NadeemGulam@users.noreply.github.com>
2024-08-04 23:55:26 +0530

    Basic Setup and DB Connection setup

---
+++ src/main/java/com/stream/app/service/VideoService.java
@@ -0,0 +1,22 @@
+package com.stream.app.service;
+
+import com.stream.app.entities.Video;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface VideoService {
+//    Save video
+
+    Video save(Video video, MultipartFile file);
+
+    //    Get Video by ID
+    Video get(String videoId);
+
+//    Get Video By Title
+
+    Video getByTitle(String title);
+
+    List<Video> getALl();
+}
+
